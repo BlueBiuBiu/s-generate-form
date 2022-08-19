325e97179da13839fd24f8ee22331792b303a46a
Lijingwei <lijw@miduonet.com>
2022-08-19 14:57:10 +0800

    fix: 包版本变更

--- src/App.vue
+++ src/App.vue
@@ -11,11 +11,11 @@
 
 <script setup lang="ts">
 import json from './components/config.js'
-// import formGenerate from './components/FormGenerate.vue'
+// import SkyGenerateForm from './components/FormGenerate.vue'
 // import SkyGenerateForm from '../dist/s-generate-form.es.js'
 // import "../dist/style.css"
-import  SkyGenerateForm from 'sky-generate-form'
 import "sky-generate-form/style.css"
+import  SkyGenerateForm from 'sky-generate-form'
 
 const test = ref()
 
